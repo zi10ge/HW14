a306c737fe54be3cc785e11f3649103ef623539b
zi10ge <zi10ge@gmail.com>
2020-10-28 10:17:45 +0300

    test

--- config.tf
+++ config.tf
@@ -77,11 +77,11 @@ resource "aws_security_group" "ubuntu" {
   }
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "build_instance" {
   ami                    = "${var.image_id}"
   instance_type          = "t2.micro"
-#  vpc_security_group_ids = ["${aws_security_group.ubuntu.id}"]
-#  subnet_id              = "${var.subnet_id}"
+  vpc_security_group_ids = ["${aws_security_group.ubuntu.id}"]
+  subnet_id              = "${var.subnet_id}"
   key_name               = "${aws_key_pair.generated_key.key_name}"
   count                  = 1
 #  associate_public_ip_address = true 
