1c03ee8835ce50da0641c13858418fd114758285
zi10ge <zi10ge@gmail.com>
2020-10-28 10:00:23 +0300

    test

--- config.tf
+++ config.tf
@@ -72,10 +72,10 @@ resource "aws_security_group" "ubuntu" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami           = "${var.image_id}"
-  instance_type = "t2.micro"
+  ami                    = "${var.image_id}"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.ubuntu.id}"]
-  subnet_id = "${var.subnet_id}"
-  key_name      = "${aws_key_pair.generated_key.key_name}"
-  user_data = <<EOF
+  subnet_id              = "${var.subnet_id}"
+  key_name               = "${aws_key_pair.generated_key.key_name}"
+#  user_data = <<EOF
 }
